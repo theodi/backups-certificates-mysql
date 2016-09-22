f17ca0288891203b64bd6cba8c953864d39dddb6
pikesley <github@orgraphone.org>
2016-09-22 17:08:20 +0100

    More tests

--- spec/mysql_spec.rb
+++ spec/mysql_spec.rb
@@ -15,5 +15,43 @@ describe 'Test MySQL restore' do
         expect(@tables).to include 'kitten_data'
       end
     end
+
+    context 'users' do
+      before :all do
+        @users = @client.query 'SELECT * FROM users'
+      end
+
+      it 'has plenty users' do
+        expect(@users.count).to be > 7183
+      end
+
+      it 'has the correct users' do
+        expect(@users.map { |u| u['email'] }[7]).to eq 'jeni@theodi.org'
+      end
+    end
+
+    context 'certificates' do
+      before :all do
+        @certs = @client.query 'SELECT * FROM certificates'
+      end
+
+      it 'has thousands of certificates' do
+        expect(@certs.count).to be > 223465
+      end
+
+      it 'has the correct certificates' do
+        expect(@certs.map { |c| c['curator'] }[40]).to eq 'Ulrich Atz'
+      end
+    end
+
+    context 'datasets' do
+      before :all do
+        @sets = @client.query 'SELECT * FROM datasets'
+      end
+
+      it 'has thousands of datasets' do
+        expect(@sets.count).to be > 210276
+      end
+    end
   end
 end
